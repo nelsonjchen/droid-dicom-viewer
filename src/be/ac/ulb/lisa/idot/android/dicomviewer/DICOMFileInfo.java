64acee488874b2187541768d137d29233750d93b
Nelson Chen <crazysim@gmail.com>
2011-03-13 16:15:48 -0700

    make activity scaffold

--- src/be/ac/ulb/lisa/idot/android/dicomviewer/DICOMFileInfo.java
+++ src/be/ac/ulb/lisa/idot/android/dicomviewer/DICOMFileInfo.java
@@ -6,5 +6,6 @@ import android.os.Bundle;
 public class DICOMFileInfo extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.dicom_info_viewer);
     }
 }
