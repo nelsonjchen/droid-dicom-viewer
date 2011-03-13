084517671c328aac9326fc8c717608b40f17c634
Nelson Chen <crazysim@gmail.com>
2011-03-13 15:55:35 -0700

    dicom status files

---
+++ src/be/ac/ulb/lisa/idot/android/dicomviewer/DICOMFileInfo.java
@@ -0,0 +1,10 @@
+package be.ac.ulb.lisa.idot.android.dicomviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DICOMFileInfo extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
