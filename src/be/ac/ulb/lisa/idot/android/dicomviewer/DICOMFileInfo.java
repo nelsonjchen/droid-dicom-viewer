3e9543bb9526d5b2787bc168b5510595bff0f3e2
Nelson Chen <crazysim@gmail.com>
2011-03-13 16:39:46 -0700

    made to listactivity

--- src/be/ac/ulb/lisa/idot/android/dicomviewer/DICOMFileInfo.java
+++ src/be/ac/ulb/lisa/idot/android/dicomviewer/DICOMFileInfo.java
@@ -1,11 +1,14 @@
 package be.ac.ulb.lisa.idot.android.dicomviewer;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class DICOMFileInfo extends Activity {
+public class DICOMFileInfo extends ListActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dicom_info_viewer);
+        int a = 0;
+
     }
 }
