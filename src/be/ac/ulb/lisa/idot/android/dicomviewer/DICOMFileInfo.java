4d7093c1751f8fc10d64a4e84760fede2caed167
Nelson Chen <crazysim@gmail.com>
2011-03-13 17:05:09 -0700

    test hash map

--- src/be/ac/ulb/lisa/idot/android/dicomviewer/DICOMFileInfo.java
+++ src/be/ac/ulb/lisa/idot/android/dicomviewer/DICOMFileInfo.java
@@ -1,14 +1,22 @@
 package be.ac.ulb.lisa.idot.android.dicomviewer;
 
-import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 public class DICOMFileInfo extends ListActivity {
+
+    private static LinkedHashMap<String,String> map ;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dicom_info_viewer);
-        int a = 0;
+        map = new LinkedHashMap<String, String>();
+        map.put("lol", "lol");
+        map.put("2nd amendment","AMENDMENTED");
+
 
     }
 }
