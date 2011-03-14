54abe19ae8e60ed8f6c43d56361cdb42f8cc5258
Nelson Chen <crazysim@gmail.com>
2011-03-13 17:36:46 -0700

    removed assumptions about implementations for now

--- src/be/ac/ulb/lisa/idot/android/dicomviewer/DICOMFileInfo.java
+++ src/be/ac/ulb/lisa/idot/android/dicomviewer/DICOMFileInfo.java
@@ -3,20 +3,14 @@ package be.ac.ulb.lisa.idot.android.dicomviewer;
 import android.app.ListActivity;
 import android.os.Bundle;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-
 public class DICOMFileInfo extends ListActivity {
 
-    private static LinkedHashMap<String,String> map ;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dicom_info_viewer);
-        map = new LinkedHashMap<String, String>();
-        map.put("lol", "lol");
-        map.put("2nd amendment","AMENDMENTED");
 
 
     }
+
 }
