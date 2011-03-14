579f222090b121dc05555f94b209df172e26fedf
Nelson Chen <crazysim@gmail.com>
2011-03-14 01:29:57 -0700

    dicom stuff

--- src/be/ac/ulb/lisa/idot/android/dicomviewer/DICOMFileInfo.java
+++ src/be/ac/ulb/lisa/idot/android/dicomviewer/DICOMFileInfo.java
@@ -2,19 +2,24 @@ package be.ac.ulb.lisa.idot.android.dicomviewer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import be.ac.ulb.lisa.idot.dicom.DICOMException;
 import be.ac.ulb.lisa.idot.dicom.data.DICOMMetaInformation;
 import be.ac.ulb.lisa.idot.dicom.file.DICOMReader;
 import org.dcm4che2.data.BasicDicomObject;
+import org.dcm4che2.data.Tag;
+import org.dcm4che2.data.VR;
+import org.dcm4che2.io.DicomInputHandler;
 import org.dcm4che2.io.DicomInputStream;
 
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class DICOMFileInfo extends Activity {
-
+public class DICOMFileInfo extends Activity implements DicomInputHandler {
+    ArrayList<RowModel> information;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +31,11 @@ public class DICOMFileInfo extends Activity {
             DicomInputStream dis = new DicomInputStream(file);
 
             BasicDicomObject bdo = new BasicDicomObject();
-            dis.readDicomObject(bdo,-1);
+            dis.setHandler(this);
+            dis.readDicomObject(bdo, -1);
+            information = new ArrayList<RowModel>();
+
+
 
             dis.close();
 
@@ -41,5 +50,39 @@ public class DICOMFileInfo extends Activity {
         finish();
 
     }
+
+    @Override
+    public boolean readValue(DicomInputStream in) throws IOException {
+        switch (in.tag()){
+            case Tag.Item:
+//                if (in.sq().vr() != VR.SQ && in.valueLength() != -1) {
+//                    outFragment(in);
+//                } else {
+//                    outItem(in);
+//                }
+                break;
+            case Tag.ItemDelimitationItem:
+            case Tag.SequenceDelimitationItem:
+//                if (in.level() > 0)
+//                    outItem(in);
+                break;
+            default:
+                outElement(in);
+        }
+
+        return true;
+
+    }
+
+    public void outElement(DicomInputStream in) {
+
+        return;
+    }
+
+    class RowModel {
+        String description;
+        String value;
+    }
+
 
 }
